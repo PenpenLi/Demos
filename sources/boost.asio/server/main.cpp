4e4cf67fa5311e557713839941ceb2c45afc82b9
Xiaobin Li <xl@MrL-MBP.local>
2015-05-25 12:47:59 +0800

    phoenix

--- sources/boost.asio/server/main.cpp
+++ sources/boost.asio/server/main.cpp
@@ -4,7 +4,6 @@
  *  Created on: 2014-3-4
  *      Author: xiaobin
  */
-
 #include <boost/thread.hpp>
 #include "memerypool.hpp"
 #include "server.h"
@@ -12,21 +11,28 @@
 #define LOG_TAG "main"
 #include "XLLogger.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	int result = 0;
-	try {
+	try
+    {
 		XL::Logger::Instance()->InitLogger(argv[0]);
 		XL::Logger::Instance()->SetLogLevel(XL::Logger::LogLevelDebug);
 		server s(44444, boost::thread::hardware_concurrency() + 2);
 		s.run();
-	} catch (const std::exception& e) {
+	}
+    catch (const std::exception& e)
+    {
 		LOGE("Catch exception: "<<e.what());
 		result = 1;
-	} catch (...) {
+	}
+    catch (...)
+    {
 		LOGE("Catch unknown exception!");
 		result = -1;
 	}
 
 	memory_pool_8k::purge_memory();
+
 	return result;
 }
