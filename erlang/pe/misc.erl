5fbed1aeb1205e69bc75c54cc32a98fb7c0e624d
Xiaobin Li <l_xb@foxmail.com>
2015-06-29 23:39:52 +0800

    my_time_func

--- erlang/pe/misc.erl
+++ erlang/pe/misc.erl
@@ -1,7 +1,16 @@
 -module(misc).
 -export([my_tuple_to_list/1]).
+-export([my_time_func/1]).
 
 my_tuple_to_list(T) -> tuple_to_list_helper(T, erlang:size(T), []).
+
+my_time_func(F) ->
+    {_, S1, MS1} = erlang:now(),
+    F(),
+    {_, S2, MS2} = erlang:now(),
+    Result = (S2 - S1) + (MS2 - MS1) / 1000.0,
+    Result.
+
 
 tuple_to_list_helper(_, 0, List) -> List;
 tuple_to_list_helper(T, N, List) -> tuple_to_list_helper(T, N - 1, List) ++ [erlang:element(N, T)].
