fe12acca7562326da240b58964e312eb7d0d257f
xl000 <l_xb@foxmail.com>
2019-07-02 12:05:40 +0800

    fk

---
+++ python/kdygfk/dec_img.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# _*_ coding: utf-8 _*_
+
+import os
+import zlib
+
+def decrypt(path):
+    f = open(path, "rb")
+    content = f.read()
+    f.close()
+    try:
+        dec = zlib.decompress(content, zlib.MAX_WBITS)
+        fout = open(path, "wb")
+        fout.write(dec)
+        fout.close()
+    except:
+        print "error"
+        pass
+
+def main():
+    for root, dirs, files in os.walk('./abc_assets/'):
+        for f in files:
+            if f.endswith('.mp3') or f.endswith('.jpg') or f.endswith('.png') or f.endswith('.xml') or f.endswith('.bytes') or f.endswith('.atf') or f.endswith('.json'):
+                print "decrypt " + os.path.join(root, f)
+                decrypt(os.path.join(root, f))
+
+if __name__ == '__main__':
+    main()
